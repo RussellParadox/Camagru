70ba5af2de3477b048143410a74c1bf7987c1628
Geoffrey Dornic <gdornic.pro@gmail.com>
2025-04-19 13:39:36 +0200

    dropdown menu now close when click on a link

--- sources/mapped_volumes/frontend/scripts/events/dropdown.js
+++ sources/mapped_volumes/frontend/scripts/events/dropdown.js
@@ -1,5 +1,6 @@
 const settings_dropdown = document.getElementById("settings-dropdown");
 const navbar_settings = document.getElementById("navbar-settings");
+const settings_dropdown_content = settings_dropdown.querySelectorAll("a");
 
 navbar_settings.addEventListener("mousemove", (e) => {
     settings_dropdown.style.display = "block";
@@ -15,4 +16,10 @@ navbar_settings.addEventListener("mouseleave", () => {
 
 settings_dropdown.addEventListener("mouseleave", () => {
     settings_dropdown.style.display = "none";
+});
+
+settings_dropdown_content.forEach((a) => {
+    a.addEventListener("click", (e) => {
+        settings_dropdown.style.display = "none";
+    });
 });
