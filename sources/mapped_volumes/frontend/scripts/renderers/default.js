804c3e0d2d9be152643023cbd2dd0f0a29959a02
Geoffrey Dornic <gdornic.pro@gmail.com>
2025-04-19 14:32:45 +0200

    fixed gear box size

--- sources/mapped_volumes/frontend/scripts/renderers/default.js
+++ sources/mapped_volumes/frontend/scripts/renderers/default.js
@@ -1,3 +1,3 @@
 export function renderer(template) {
-    return template;
+    return template.innerHTML;
 }
--- sources/mapped_volumes/frontend/scripts/renderers/home.js
+++ sources/mapped_volumes/frontend/scripts/renderers/home.js
@@ -1,24 +1,33 @@
 const example_data = [
     {
-        "source": './images/example.jpeg',
-        "caption": 'My professional picture',
+        "source": './images/example_picture.jpeg',
+        "caption": 'My professional picture jkfdklfaklsfjadslkfjadsklfjdslafkjdslkfjadsklfjlkdsjfkladsjfklajflkdsjflkasdjflkdasjflkadsjfkldasjfkladsjfkladsjflkadsjfklasdjfdsklafjadsklfjadsklfjksldafjaksl',
         "like_quantity": '69',
         "comment_quantity": '45',
         "creation_date": '21/02/2023',
+        "comments": [
+            {"user": "Someone", "comment": "Hey what's up man ? Nice picture."},
+            {"user": "Angry_man", "comment": "You stole my picture, I'll find you and I'll kill you."},
+            {"user": "Someone_else", "comment": "Can you answer one day ?"},
+        ],
     },
     {
         "source": './images/example.jpeg',
         "caption": 'My professional picture',
-        "like_quantity": '69',
-        "comment_quantity": '45',
-        "creation_date": '21/02/2023',
+        "like_quantity": '6997',
+        "comment_quantity": '5',
+        "creation_date": '01/02/2025',
+        "comments": [
+            {"user": "Angry_man", "comment": "litteraly the best buddy ever"},
+        ],
     },
     {
-        "source": './images/example.jpeg',
+        "source": './images/example_picture.jpeg',
         "caption": 'My professional picture',
         "like_quantity": '69',
         "comment_quantity": '45',
         "creation_date": '21/02/2023',
+        "comments": [],
     },
 ];
 
