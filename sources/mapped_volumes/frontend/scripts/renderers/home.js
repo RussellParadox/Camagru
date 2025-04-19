4be152352e1a295f39da0e45768e6e5725d5c075
Geoffrey Dornic <gdornic.pro@gmail.com>
2025-04-19 15:54:11 +0200

    truncate large numbers

--- sources/mapped_volumes/frontend/scripts/renderers/home.js
+++ sources/mapped_volumes/frontend/scripts/renderers/home.js
@@ -2,8 +2,8 @@ const example_data = [
     {
         "source": './images/example_picture.jpeg',
         "caption": 'My professional picture jkfdklfaklsfjadslkfjadsklfjdslafkjdslkfjadsklfjlkdsjfkladsjfklajflkdsjflkasdjflkdasjflkadsjfkldasjfkladsjfkladsjflkadsjfklasdjfdsklafjadsklfjadsklfjksldafjaksl',
-        "like_quantity": '69',
-        "comment_quantity": '45',
+        "like_quantity": 69,
+        "comment_quantity": 45,
         "creation_date": '21/02/2023',
         "comments": [
             {"user": "Someone", "comment": "Hey what's up man ? Nice picture."},
@@ -14,8 +14,8 @@ const example_data = [
     {
         "source": './images/example.jpeg',
         "caption": 'My professional picture',
-        "like_quantity": '6997',
-        "comment_quantity": '5',
+        "like_quantity": 6997000,
+        "comment_quantity": 5000000000,
         "creation_date": '01/02/2025',
         "comments": [
             {"user": "Angry_man", "comment": "litteraly the best buddy ever"},
@@ -24,12 +24,27 @@ const example_data = [
     {
         "source": './images/example_picture.jpeg',
         "caption": 'My professional picture',
-        "like_quantity": '69',
-        "comment_quantity": '45',
+        "like_quantity": 69,
+        "comment_quantity": 45,
         "creation_date": '21/02/2023',
         "comments": [],
     },
 ];
+
+const numeric_shorthands = {
+    1000000000: 'B',
+    1000000: 'M',
+    1000: 'K',
+}
+
+function truncate_number(number) {
+    if (number < 1000) {
+        return number;
+    }
+    const key = Object.keys(numeric_shorthands).map((n) => parseInt(n)).reverse().find((n) => Math.floor(number / n) > 0);
+    const shorthand = numeric_shorthands[String(key)];
+    return String(Math.floor(number / key)) + shorthand;
+}
 
 export function renderer(template) {
     let home_gallery = template.getElementById("home-gallery");
@@ -43,8 +58,8 @@ export function renderer(template) {
         gallery_post.querySelector('.post-caption').querySelector('time').textContent = item.creation_date;
 
         let post_engagement_quantitys = gallery_post.querySelector('.post-engagement').querySelectorAll('p');
-        post_engagement_quantitys[0].textContent = item.like_quantity;
-        post_engagement_quantitys[1].textContent = item.comment_quantity;
+        post_engagement_quantitys[0].textContent = truncate_number(item.like_quantity);
+        post_engagement_quantitys[1].textContent = truncate_number(item.comment_quantity);
 
         home_gallery.appendChild(gallery_post);
     });
